8d99129ad568909c987550e6e4212f3fa5e4081a
Meg Cunningham <meg_cunningham@live.com>
2015-09-02 14:58:29 -0500

    fixed spacing error for part 4 in views.py file

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -22,5 +22,5 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 def vote(request, question_id):
-	model = Question
+    model = Question
     template_name = 'polls/results.html'
