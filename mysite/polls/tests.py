44234901d846323dbe883a231f54d2525e3e1976
Meg Cunningham <meg_cunningham@live.com>
2015-09-03 12:42:02 -0500

    fixed part 5 error like a boss

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django.test import timezone
+from django.utils import timezone
 from django.test import TestCase
 
 from .models import Question
@@ -12,7 +12,7 @@ class QuestionMethodTests(TestCase):
 	def test_was_published_recently_with_future_question(self):
 		time = timezone.now() + datetime.timedelta(days=30)
 		future_question = Question(pub_date=time)
-		self.assertEqual(future_question.test_was_published_recently(), False)
+		self.assertEqual(future_question.was_published_recently(), False)
 
 
 
